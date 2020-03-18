487cc2252dd98356796c8e4a0aa2d7a249e43f68
relecteve <Filonovaval@gmail.com>
2020-03-18 09:47:40 +0300

    Исправляет 17 баг

--- build/js/mask-tell.js
+++ build/js/mask-tell.js
@@ -3,25 +3,25 @@
 'use strict';
 
 (function () {
-  var setupPhone = document.querySelector('.js__input--tel');
-  setupPhone.addEventListener('focus', function () {
-    if (setupPhone.value === '') {
-      setupPhone.value = '+7(';
-    }
+  // var setupPhone = document.querySelector('.js__input--tel');
+  // setupPhone.addEventListener('focus', function () {
+  //   if (setupPhone.value === '') {
+  //     setupPhone.value = '+7(';
+  //   }
 
-  });
+  // });
 
-  window.setInterval(function () {
-    if (setupPhone.value.length === 6) {
-      setupPhone.value = setupPhone.value + ')';
-    }
-    if (setupPhone.value.length === 10) {
-      setupPhone.value = setupPhone.value + '-';
-    }
-    if (setupPhone.value.length === 13) {
-      setupPhone.value = setupPhone.value + '-';
-    }
-  }, 100);
+  // window.setInterval(function () {
+  //   if (setupPhone.value.length === 6) {
+  //     setupPhone.value = setupPhone.value + ')';
+  //   }
+  //   if (setupPhone.value.length === 10) {
+  //     setupPhone.value = setupPhone.value + '-';
+  //   }
+  //   if (setupPhone.value.length === 13) {
+  //     setupPhone.value = setupPhone.value + '-';
+  //   }
+  // }, 100);
 
   var maskedInputs = document.querySelectorAll('input[data-inputmask]');
 
